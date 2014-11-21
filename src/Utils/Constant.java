339190b24516a8cc57f8585cef0f517f64411d66
madiska <madiskangro@gmail.com>
2014-11-21 11:43:22 +0200

    wasa

--- src/Utils/Constant.java
+++ src/Utils/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
 	public static final String ExceliAsukoht = System.getProperty("user.dir")+"//src//testdata//reg.xlsx";
 	
 	
-	public static final String ChromeDriver = System.getProperty("user.dir")+"//src//Utils//chromedriver.exe";
+	public static final String ChromeDriver = System.getProperty("user.dir")+"//src//Utils//chromedriver";
 	
 	public static final String IEDriver = "C:\\Users\\Madis\\Downloads\\IEDriverServer32.exe";
 
